a979d8633e0b826eb4f8aacf5edfce6da136e115
Mamad <mohammadpjsh@gmail.com>
2025-04-04 18:00:55 +0100

    Final Product

---
+++ src/Game/Rules/SkipRule.java
@@ -0,0 +1,22 @@
+package Game.Rules;
+
+
+import Board.*;
+import Game.Player;
+
+public class SkipRule implements PlayerMovementRules {
+    @Override
+    public void applyRule(Player player, Position newPosition, Position currentPosition, Board board) {
+        int nextPosNum = ((newPosition.getNumber()) % board.getBoardSize()) + 1;
+        Position nextPos = board.getPosition(nextPosNum);
+        if (nextPos.getType() == PositionType.TAIL && nextPos.getOwner() != player.getColor()) {
+            int skipAmount = board.getTailLength();
+            System.out.println(player.getColor() + " skips " + nextPos.getOwner() + "'s tail.");
+            //set skip amount to tail length
+            player.setSkip(skipAmount);
+        } else {
+            //set skip amount to 0
+            player.setSkip(0);
+        }
+    }
+}
